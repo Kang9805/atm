5e09f0214178ecb6510e1d9c0233e62fdc2da6d8
kang9805 <zzionkk@naver.com>
2025-07-18 23:00:58 +0900

    ✨ 에러 처리
    입력된 값이 숫자가 아니거나 음수일 경우 또는 잘못된 메뉴 선택시 경고 메세지 출력

--- atm.py
+++ atm.py
@@ -11,24 +11,35 @@ receipts = []
 while True:
     num = input("사용하실 기능의 번호를 입력해주세요. (1.입금, 2.출금, 3.영수증 보기, 4.종료) : ")
 
-    if num == '4': # 4번은 종료 기능
-        break # while문 탈출
-    
+   
     if num == '1': #입금 기능 구현 -> feat/deposit 브랜치에서 작업
-       deposit_amount = int(input('입금할 금액을 입력해주세요: ')) #str:5000 -> int:5000
-       balance += deposit_amount # balance = balance + deposit_amount
-       print(f'입금하신 금액은{deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')    
-       # 보안 문제
-       receipts.append(('입금', deposit_amount, balance))
+        try:
+            deposit_amount = int(input('입금할 금액을 입력해주세요: ')) #str:5000 -> int:5000
+            if deposit_amount <= 0:
+                print("0 이하의 금액은 입력할 수 없습니다.")
+                continue
+            balance += deposit_amount # balance = balance + deposit_amount
+            print(f'입금하신 금액은{deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')    
+            receipts.append(('입금', deposit_amount, balance))
+        except ValueError:
+            print("숫자만 입력해주세요")
+
+        
 
     if num == '2':
-        withdraw_amount = int(input('출금할 금액을 입력해주세요:'))
-        withdraw_amount = min(balance,withdraw_amount)
-        balance -= withdraw_amount # balance = balance - deposit_amount
-        print(f'출금하신 금액은{withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
-        receipts.append(('출금', withdraw_amount, balance))
+        try:
+            withdraw_amount = int(input('출금할 금액을 입력해주세요:'))
+            if withdraw_amount <= 0:
+                print("0 이하의 금액을 입력할 수 없습니다.")
+                continue
+            withdraw_amount = min(balance,withdraw_amount)
+            balance -= withdraw_amount # balance = balance - deposit_amount
+            print(f'출금하신 금액은{withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+            receipts.append(('출금', withdraw_amount, balance))
+        except ValueError:
+            print("숫자만 입력해주세요")
     
-    if num == '3':
+    elif num == '3':
         if receipts:
             for i in receipts:
             # i = ('입금'. 3000. 13000)
@@ -36,4 +47,12 @@ while True:
                 print(f'{i[0]}: {i[1]}원 | 잔액: {i[2]}')
         else:
             print('영수증 내역이 없습니다.')    
-print(f'서비스를 종료합니다. 햔재 잔액은 {balance}원 입니다.'4
+    
+    elif num == '4': # 4번은 종료 기능
+        break # while문 탈출
+    
+
+    else:
+        print("유효하지 않은 선택입니다. 1, 2, 3, 4 중 하나를 입력해주세요")
+
+print(f'서비스를 종료합니다. 햔재 잔액은 {balance}원 입니다.')
